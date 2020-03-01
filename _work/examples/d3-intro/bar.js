49eb57905303170ef090f73260789c41806606b4
Lenny Martinez <lennymartinezd@gmail.com>
2020-03-01 15:21:57 -0500

    start bar chart code

--- _work/examples/d3-intro/bar.js
+++ _work/examples/d3-intro/bar.js
@@ -1,3 +1,4 @@
+// This first section is all about setting up our chart in terms of 
 var margin = {
 	top: 20, 
 	right: 20, 
@@ -9,3 +10,20 @@ var width = 800,
 	height = 400,
 	w = width + margin.left + margin.right,
 	h = height + margin.top + margin.bottom;
+
+
+// we can use a rowConverter to ensure the data we load is properly formatted.
+var rowConverter = function(d) {
+	return {
+		genre: d.genre,
+		votes: +d.count
+	}
+}
+
+d3.csv("data.csv", rowConverter)
+.then(function(data) {
+	// all your chart making goes here. Nearly everything that worked in d3v4 will work in d3v5
+
+	// We can check that our data loaded!
+	console.log(data);
+})
